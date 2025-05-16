ed95ddd3fdd9391d779868356d49a32499049da8
Seu Nome <seu.email@exemplo.com>
2025-05-16 17:28:22 -0300

    Commit inicial do projeto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Calendar } from 'react-big-calendar'
+import { DatePicker } from '@mui/x-date-pickers'
+import { useSpeechRecognition } from 'react-speech-kit'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>RealYou</h1>
+      <p>Bem-vindo ao seu novo projeto!</p>
+    </div>
+  )
+}
+
+export default App 
